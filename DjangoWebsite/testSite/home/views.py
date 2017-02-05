8041b72464286435f4c73423e8b8ef5a8b156a2e
Vighnesh Raman <vighneshraman@gmail.com>
2017-02-05 00:06:10 -0500

    Commented out code under views.py for the home page of the django site.

--- DjangoWebsite/testSite/home/views.py
+++ DjangoWebsite/testSite/home/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework import viewsets
-from .serializers import UserSerializer, GroupSerializer
+# from .serializers import UserSerializer, GroupSerializer
 
 # API Serializers
-class UserViewSet(viewsets.ModelViewSet):
-    """API endpoint that allows users to be viewed or edited."""
-    queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = UserSerializer
+# class UserViewSet(viewsets.ModelViewSet):
+    # """API endpoint that allows users to be viewed or edited."""
+    # queryset = User.objects.all().order_by('-date_joined')
+    # serializer_class = UserSerializer
 
-class GroupViewSet(viewsets.ModelViewSet):
-    """API endpoint that allows groups to be viewed or edited."""
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
+# class GroupViewSet(viewsets.ModelViewSet):
+    # """API endpoint that allows groups to be viewed or edited."""
+    # queryset = Group.objects.all()
+    # serializer_class = GroupSerializer
 
 # Create your views here.
 def home(request):
